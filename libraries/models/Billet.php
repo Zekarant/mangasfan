4674c6d876a9b3f1d399bbed2ff188f834e6cd74
CHOMEL <colin42660@gmail.com>
2020-03-25 20:37:11 +0100

    Ajout d'informations pour les news

--- libraries/models/Billet.php
+++ libraries/models/Billet.php
@@ -5,6 +5,6 @@ namespace Models;
 class Billet extends Model {
 
 	protected $table = "billets b";
-	protected $selection = "b.id AS id_news, b.titre, b.description AS description_news, b.contenu, b.theme, b.auteur, b.date_creation, b.slug, u.id AS id_user, u.username";
+	protected $selection = "b.id AS id_news, b.titre, b.description AS description_news, b.contenu, b.categorie, b.theme, b.auteur, b.date_creation, b.slug, u.id AS id_user, u.username, u.avatar, u.role";
 	
 }
--- templates/billets/show.html.php
+++ templates/billets/show.html.php
@@ -3,6 +3,47 @@
 <?= $billet['contenu'] ?>
 <p class="auteur-news"><small>News rédigée le <?= $billet['date_creation'] ?> par <a href="#"><?= $billet['username'] ?></a></small></p>
 <hr>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-5">
+            <div class="card" style="width: 25rem;">
+              <div class="card-header">
+                A propos de l'auteur
+            </div>
+            <div class="bloc-auteur">
+                <div class="row">
+                    <div class="col-lg-3">
+                        <img src="https://www.mangasfan.fr/membres/images/avatars/<?= $billet['avatar']; ?>" alt="Avatar de <?= $billet['username'] ?>" class="auteur-avatar" />
+                    </div>
+                    <div class="col-lg-9 a-propos-auteur">
+                        <h5><?= $billet['username']; ?></h5>
+                        <hr>
+                        <small><i>« <?= $billet['role']; ?> »</i></small>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3">
+         <div class="col-lg-4">
+            <div class="card" style="width: 18rem;">
+              <div class="card-header">
+                A propos de la news
+            </div>
+            <div class="bloc-auteur">
+                <?php if($billet['categorie'] != "Site"){ ?>
+                    Cette news appartient à la catégorie « <a href="#"><strong><?= $billet['categorie']; ?></strong></a> ».<br/>
+                    Cliquez sur le lien ci-dessus pour accéder à ces derniers.
+                <?php } else { ?>
+                    Cette news appartient à la catégorie « <strong>Site</strong> »
+                <?php } ?>
+            </div>
+        </div>
+    </div>
+    </div>
+</div>
+</div>
+<hr>
 <h2>Espace commentaires :</h2>
 <?php if (count($commentaires) === 0): ?>
     <div class="alert alert-primary" role="alert">Cet article n'a pas encore de commentaire ! N'hésitez pas à en poster !</div>
