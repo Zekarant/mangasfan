2446d5c14b62078f3ca5d82709f096f42348aaef
CHOMEL <colin42660@gmail.com>
2021-01-16 19:11:52 +0100

    Mangas'Fan 7.5 Symfony - Vu des news

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\News;
 use App\Repository\NewsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,9 +13,16 @@ class NewsController extends AbstractController
     /**
      * @Route("/", name="app_home")
      */
-    public function index(NewsRepository $newsRepository)
+    public function index(NewsRepository $newsRepository) : Response
     {
         $news = $newsRepository->findAll();
         return $this->render('news/index.html.twig', compact('news'));
+    }
+
+    /**
+     * @Route("/news/{id<[0-9]+>}", name="app_news_show")
+     */
+    public function show(News $news) : Response {
+        return $this->render('news/show.html.twig', compact('news'));
     }
 }
