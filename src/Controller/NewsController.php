727c329da1e2064fd5ce5b4610b13d178d71f475
CHOMEL <colin42660@gmail.com>
2021-01-16 18:50:50 +0100

    News de MF version Symfony

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -2,19 +2,19 @@
 
 namespace App\Controller;
 
-use App\Repository\PinRepository;
+use App\Repository\NewsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class PinsController extends AbstractController
+class NewsController extends AbstractController
 {
     /**
      * @Route("/", name="app_home")
      */
-    public function index(PinRepository $pinRepository)
+    public function index(NewsRepository $newsRepository)
     {
-        $pins = $pinRepository->findAll();
-        return $this->render('pins/index.html.twig', ['pins' => $pins]);
+        $news = $newsRepository->findAll();
+        return $this->render('news/index.html.twig', compact('news'));
     }
 }
--- src/Entity/News.php
+++ src/Entity/News.php
@@ -2,14 +2,13 @@
 
 namespace App\Entity;
 
-use App\Repository\PinRepository;
+use App\Repository\NewsRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity(repositoryClass=PinRepository::class)
- * @ORM\Table(name="pins")
+ * @ORM\Entity(repositoryClass=NewsRepository::class)
  */
-class Pin
+class News
 {
     /**
      * @ORM\Id
@@ -24,9 +23,14 @@ class Pin
     private $title;
 
     /**
-     * @ORM\Column(type="text")
+     * @ORM\Column(type="string", length=255)
      */
     private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image;
 
     public function getId(): ?int
     {
@@ -53,6 +57,18 @@ class Pin
     public function setDescription(string $description): self
     {
         $this->description = $description;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
 
         return $this;
     }
